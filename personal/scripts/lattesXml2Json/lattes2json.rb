d81882161c89c3da7dd790bd9e2507a439d73393
pauloborba <phmb@cin.ufpe.br>
2026-05-30 18:29:18 -0300

    bug fix: nil author names properly handled now

--- personal/scripts/lattesXml2Json/lattes2json.rb
+++ personal/scripts/lattesXml2Json/lattes2json.rb
@@ -39,13 +39,20 @@ def includeFile(fileName)
   end
 end
 
-def processaListaNomes(autores)
-	if autores.size == 0 then
+def processaListaNomes(autores)	
+  if autores.size == 0 then
 		"\"\""
-	elsif autores.size == 1 then
-		"\"" + autores[0] + "\""
-	else
-		("\"" + autores[0] + "\"") << ", " << processaListaNomes(autores - [autores[0]])
+	else 
+    if autores[0] != nil 
+      autor = autores[0]
+    else 
+      autor = ""
+    end
+    if autores.size == 1 then
+		  "\"" + autor + "\""
+	  else
+		  ("\"" + autor + "\"") << ", " << processaListaNomes(autores - [autores[0]])
+    end
   end
 end
 
@@ -718,7 +725,7 @@ end
 
 vitae = REXML::Document.new(File.open("vitaeCNPq.xml"))
 anos =
-["2024","2023","2022","2021","2020","2019","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991","1990","1989","1988","1987","1986"]
+["2026","2025","2024","2023","2022","2021","2020","2019","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991","1990","1989","1988","1987","1986"]
 referencia = 22
 
 # Instruções de uso:
